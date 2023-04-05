f2ca78398ce9d39a1304ec79c981f824131d8ca2
Punit <erpunitthakkar@gmail.com>
2023-04-05 12:45:26 +0530

    stop added in ee

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -1,5 +1,9 @@
 public class ElectricEngine {    
-public void start() {
-    System.out.println("ElectricEngine started");
-}
+    public void start() {
+        System.out.println("ElectricEngine started");
+    }
+
+    public void stop() {
+        System.out.println("ElectricEngine stoped");
+    }
 }
