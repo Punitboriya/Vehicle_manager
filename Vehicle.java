8a12c4b25d9ed366575a89c935f640f43c34c6f5
Punit <erpunitthakkar@gmail.com>
2023-04-05 11:48:58 +0530

    Main class changes

--- Vehicle.java
+++ Vehicle.java
@@ -1,13 +1,16 @@
 
 public class Vehicle {
 	public static void main(String[] args) {
-		PetrolEngine pE = new PetrolEngine();
-		pE.start();
-		pE.stop();
-		pE.sound();
-
-		DesileEngine dE = new DesileEngine();
-		dE.start();
-		dE.stop();
+		
+		System.out.println("Hello");
+		
+//		PetrolEngine pE = new PetrolEngine();
+//		pE.start();
+//		pE.stop();
+//		pE.sound();
+//
+//		DesileEngine dE = new DesileEngine();
+//		dE.start();
+//		dE.stop();
 	}
 }
