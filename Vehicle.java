96208086beef1d134b2f3948d1c598b59d13661e
Punit <erpunitthakkar@gmail.com>
2023-04-11 11:34:14 +0530

    Tyers added

--- Vehicle.java
+++ Vehicle.java
@@ -1,9 +1,10 @@
 
 public class Vehicle {
+	static int tyers;
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello");
-		
 		PetrolEngine pE = new PetrolEngine();
 		pE.start();
 		pE.stop();
@@ -12,15 +13,17 @@ public class Vehicle {
 		DesileEngine dE = new DesileEngine();
 		dE.start();
 		dE.stop();
-		
+
 		Cng c = new Cng();
 		c.start();
-		
+
 		ElectricEngine ee = new ElectricEngine();
 		ee.start();
-		
+
 		HydrogneEngine he = new HydrogneEngine();
 		he.start();
 		he.stop();
+		tyers = 4;
+
 	}
 }
